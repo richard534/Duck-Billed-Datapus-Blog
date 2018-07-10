9e77a23815d63d2d5673e45b60cdd127942d1ec2
Richard Taylor <richard534@hotmail.co.uk>
2018-07-10 20:04:48 +0100

    Fixed html metatag charset typo

--- static.config.js
+++ static.config.js
@@ -12,7 +12,7 @@ export default {
       return (
         <Html>
           <Head>
-            <meta charset="utf-8" />
+            <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
           </Head>
           <Body>
