98b171e9f33adc888dcc9abd79a88edc5b7526cf
Richard Taylor <richard534@hotmail.co.uk>
2018-08-04 16:30:16 +0100

    Fixed code blocks on mobile view

--- static.config.js
+++ static.config.js
@@ -23,7 +23,7 @@ export default {
     <Html>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
+        <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <Body>
         {children}
